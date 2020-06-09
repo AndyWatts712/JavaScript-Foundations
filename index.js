8ab5032007c44a7a9fcd99fdf4e1b70261a878c2
megawatts68 <andrew.f.watts@gmail.com>
2020-06-09 21:08:23 +0700

    Math is working. Function created.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
+// let principal = 200000;
+// let interestRate = .05;
+// let years = 30;
+let name = "Andy";
 
 
 
@@ -14,7 +18,6 @@
 (1) Create a variable called `monthlyInterestRate` and give it the value of interest rate divided by 12. 
 (2) Create another variable called `periods` and give it the value of years*12.
 */
-
 
 
 
@@ -35,6 +38,36 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 
 When your math is correct, monthlyRate will equal 1073.64
 */
+
+let principal = 200000;
+let interestRate = .05;
+let years = 30;
+
+function mortgageCalculator(principal, interestRate, years) {
+    let monthlyInterestRate = interestRate / 12;
+    let periods = years * 12;
+
+    let n1 = Math.pow((1 + monthlyInterestRate), periods);
+    let n2 = n1 * monthlyInterestRate;
+    let numerator = n2;
+    let denominator = n1 - 1;
+    let monthlyRate = (numerator / denominator);
+    let payment = principal * monthlyRate;
+
+    return console.log(payment);
+}
+
+mortgageCalculator(200000, .05, 30);
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -65,7 +98,7 @@ mortgageCalculator(200000, 0.05, 30); <-- should return 1,073.64
 
 Then, add control flow within your function such that IF creditScore is above 740, interest rate drops by 0.5%, if credit score is below 660, interest rate increases by 0.5% and if credit score is anywhere between 660 and 740 interest rate doesn't change.
 
-Hint: To drop an interest rate by 5% you can take monthlyRate and multiply it by 0.95. Similarly, to increase an interest rate by 5% you'd do monthlyRate * 1.05. 
+Hint: To drop an interest rate by 5% you can take monthlyRate and multiply it by 0.95. Similarly, to increase an interest rate by 5% you'd do monthlyRate * 1.05.
 */
 
 
